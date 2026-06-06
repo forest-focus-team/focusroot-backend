d0822845bcf201ff528672b9f2e49fe4783d306d
bacLev206 <bacleviet43@gmail.com>
2026-06-06 23:47:48 +0700

    Create AuthController.java

---
+++ src/main/java/com/focusroot/controller/AuthController.java
@@ -0,0 +1,31 @@
+package com.focusroot.controller;
+
+import com.focusroot.dto.request.auth.LoginRequest;
+import com.focusroot.dto.request.auth.RegisterRequest;
+import com.focusroot.service.AuthService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+public class AuthController {
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<String> register(@Valid @RequestBody RegisterRequest request) {
+        authService.register(request);
+        return new ResponseEntity<>("Đăng ký thành công", HttpStatus.CREATED);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@Valid @RequestBody LoginRequest request) {
+        String token = authService.login(request);
+        return ResponseEntity.ok(token);
+    }
+}
